bfab524fb0f1cd8b6d9f4970d8ff3ac6929eaccb
Valentin vanhove <valentin.vanhove@etu.umontpellier.fr>
2022-04-26 16:37:56 +0200

    Début

--- src/main/java/fr/umontpellier/iut/vues/VueDuJeu.java
+++ src/main/java/fr/umontpellier/iut/vues/VueDuJeu.java
@@ -5,9 +5,9 @@ import javafx.scene.layout.Pane;
 
 /**
  * Cette classe correspond à la fenêtre principale de l'application.
- *
+ * <p>
  * Elle est initialisée avec une référence sur la partie en cours (Jeu).
- *
+ * <p>
  * On y définit les bindings sur les éléments internes qui peuvent changer
  * (le joueur courant, les 5 cartes Wagons visibles, les destinations lors de l'étape d'initialisation de la partie, ...)
  * ainsi que les listeners à exécuter lorsque ces éléments changent
@@ -20,13 +20,25 @@ public class VueDuJeu extends Pane {
     public VueDuJeu(IJeu jeu) {
         this.jeu = jeu;
         plateau = new VuePlateau();
+
+        //autresJoueurs = new VueAutresJoueurs();
+        //carteWagon = new VueCarteWagon();
+        //choixJoueurs = new VueChoixJoueurs();
+        joueurCourant = new VueJoueurCourant(joueurCourant.getIJoueur());
+        //choixDestination = new VueDestination();
+
+
+
         getChildren().add(plateau);
+        getChildren().add(joueurCourant);
+        //getChildren().addAll(plateau, autresJoueurs, carteWagon, choixJoueurs, joueurCourant, choixDestination);
     }
 
     public IJeu getJeu() {
         return jeu;
     }
 
-    public void creerBindings() {}
+    public void creerBindings() {
+    }
 
 }
--- src/main/java/fr/umontpellier/iut/vues/VueJoueurCourant.java
+++ src/main/java/fr/umontpellier/iut/vues/VueJoueurCourant.java
@@ -1,12 +1,47 @@
 package fr.umontpellier.iut.vues;
 
+import fr.umontpellier.iut.IDestination;
+import fr.umontpellier.iut.IJoueur;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 /**
  * Cette classe présente les éléments appartenant au joueur courant.
- *
+ * <p>
  * On y définit les bindings sur le joueur courant, ainsi que le listener à exécuter lorsque ce joueur change
  */
 public class VueJoueurCourant extends Pane {
 
+
+    private IJoueur ijoueur;    // TODO : La couleur choisi par le joueur courant designe sont AVATAR
+
+    /**
+     * Constructeur
+     */
+    public VueJoueurCourant(IJoueur ijoueur){
+        this.ijoueur = ijoueur;
+    }
+
+    public IJoueur getIJoueur(){
+        return this.ijoueur;
+    }
+    public void getAvatar(){
+
+        if(ijoueur.getCouleur().equals("BLEU")){
+            ImageView avatarBleu = new ImageView(new Image("/images/avatar-BLEU.png"));
+        }
+        if(ijoueur.getCouleur().equals("ROUGE")){
+            ImageView avatarRouge = new ImageView(new Image("/images/avatar-ROUGE.png"));
+        }
+        if(ijoueur.getCouleur().equals("VERT")){
+            ImageView avatarVert = new ImageView(new Image("/images/avatar-VERT.png"));
+        }
+        if(ijoueur.getCouleur().equals("JAUNE")){
+            ImageView avatarJaune = new ImageView(new Image("/images/avatar-JAUNE.png"));
+        }
+        if(ijoueur.getCouleur().equals("ROSE")){
+            ImageView avatarRose = new ImageView(new Image("/images/avatar-ROSE.png"));
+        }
+    }
 }
