bbfe581857e1133c41f0f9e65b521978143954a5
Valentin vanhove <valentin.vanhove@etu.umontpellier.fr>
2022-05-18 18:21:07 +0200

    TODO

--- src/main/java/fr/umontpellier/iut/vues/VueJoueurCourant.java
+++ src/main/java/fr/umontpellier/iut/vues/VueJoueurCourant.java
@@ -22,9 +22,14 @@ public class VueJoueurCourant extends VBox {
     public VueJoueurCourant() {
         this.nomJoueur = new Label();
         this.carteJoueurCourant = new VBox();
+        this.destinationJoueurCourant = new VBox(); // TODO : ADD
+
         getChildren().add(nomJoueur);
         getChildren().add(carteJoueurCourant);
+        getChildren().add(destinationJoueurCourant); // TODO : ADD
+
         carteJoueurCourant.setStyle("-fx-background-color: RED;");
+        destinationJoueurCourant.setStyle("-fx-background-color: GREEN;"); // TODO : ADD
     }
 
     public Label trouveLabelCarte(IJoueur i) {
@@ -38,23 +43,37 @@ public class VueJoueurCourant extends VBox {
         return tlc;
     }
 
+    public Label trouveLabelDest(IJoueur i) {
+        Label tlc = new Label();
+        for (Node node : destinationJoueurCourant.getChildren()) {
+            tlc = (Label) node;
+            if (tlc.equals(new Label(i.getNom()))) {
+                break;
+            }
+        }
+        return tlc;
+    }
+
 
     public void creerBindings() {
         listenerJoueurChange = (observable, oldValue, newValue) -> {
             Label carteDuJC = new Label();
-            Label Test = new Label();   //TODO : à supprimer
+            Label destinationDuJC = new Label();
 
             Platform.runLater(() -> {
                 nomJoueur.setText(newValue.getNom());
 
                 carteDuJC.setText(newValue.getCartesWagon().toString());
-                Test.setText(newValue.getDestinations().toString());
+                destinationDuJC.setText(newValue.getDestinations().toString());
 
 
-                carteJoueurCourant.getChildren().remove(trouveLabelCarte(oldValue));
-                carteJoueurCourant.getChildren().add(carteDuJC);
+                carteJoueurCourant.getChildren().remove(trouveLabelCarte(oldValue)); // supprime l'ancienne carte du joueur courant
+                carteJoueurCourant.getChildren().add(carteDuJC); // ajoute la nouvelle carte du joueur courant
 
-                System.out.println(nomJoueur.getText() + " -> " + carteDuJC.getText() + " -> " + Test.getText()); //TODO : à supprimer
+                destinationJoueurCourant.getChildren().remove(trouveLabelDest(oldValue)); // supprime l'ancienne destination du joueur courant // TODO : ADD
+                destinationJoueurCourant.getChildren().add(destinationDuJC); //ajoute la nouvelle destination du joueur courant // TODO : ADD
+
+                System.out.println(nomJoueur.getText() + " -> " + carteDuJC.getText() + " -> " + destinationDuJC.getText()); //TODO : à supprimer
             });
 
         };
